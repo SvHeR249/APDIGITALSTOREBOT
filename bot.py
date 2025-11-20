5be09e8d02841fd6efcb509f8cb3e1f57c598f58
Ozilslim <ozilslim7@gmail.com>
2025-11-20 15:12:14 +0400

    Update bot code

--- bot.py
+++ bot.py
@@ -50,8 +50,8 @@ SDG_TO_AED:float = 985
 CB_MBOK_SDG_AED = 'mbok_aed_sdg_flow'
 
 # User pays SDG, receives AED
-AED_TO_SDG_LESS:float = 940.00
-AED_TO_SDG_MORE:float = 955.00
+AED_TO_SDG_LESS:float = 950.00
+AED_TO_SDG_MORE:float = 960.00
 CB_MBOK_AED_SDG = 'mbok_sdg_aed_flow'
 
 
