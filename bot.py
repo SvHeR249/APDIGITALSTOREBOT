edde8cdd4977401d4a3ed3cc03f714687f136046
Ozilslim <ozilslim7@gmail.com>
2026-01-26 20:44:09 +0400

    Update bot code

--- bot.py
+++ bot.py
@@ -38,23 +38,23 @@ CB_BUY = 'BUY_USDT'
 #للعايز يشتري 
 
 SDG_TO_USDT:float = 3630
-SDG_TO_USDT:float = 3690
+SDG_TO_USDT:float = 3700
 AED_TO_USDT:float = 3.72
 RWF_TO_USDT:float = 1480
 
 #للعايز يبيع
-USDT_TO_SDG:float = 3555
+USDT_TO_SDG:float = 3580
 USDT_TO_AED:float = 3.6
 USDT_TO_RWF:float = 1445
 USDT_TO_RWF:float = 1430
 
 # User pays AED, receives SDG
-SDG_TO_AED:float = 1002
+SDG_TO_AED:float = 1008
 CB_MBOK_SDG_AED = 'mbok_aed_sdg_flow'
 
 # User pays SDG, receives AED
 AED_TO_SDG_LESS:float = 950.00
-AED_TO_SDG_MORE:float = 965.00
+AED_TO_SDG_MORE:float = 970.00
 CB_MBOK_AED_SDG = 'mbok_sdg_aed_flow'
 
 
